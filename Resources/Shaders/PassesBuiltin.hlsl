1802365ad87b6cfd5e9464c892d231910f765165
john.murray <jm@seaberyat.com>
2020-07-27 19:26:56 +0200

    [John Murray] Updated paths to the shaders

--- Resources/Shaders/PassesBuiltin.hlsl
+++ Resources/Shaders/PassesBuiltin.hlsl
@@ -2,7 +2,7 @@
 #define DEARIMGUI_BUILTIN_INCLUDED
 
 #include "UnityCG.cginc"
-#include "Packages/com.realgames.dear-imgui/Resources/Shaders/Common.hlsl"
+#include "Packages/com.realgames.seabery.dear-imgui/Resources/Shaders/Common.hlsl"
 
 sampler2D _Tex;
 
